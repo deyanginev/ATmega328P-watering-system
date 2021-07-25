1ff50f0d634224578931429105231522bf1834e1
Deyan Ginev <dginev@gmail.com>
2021-07-25 16:49:27 +0300

    Fix Arduino esp32 builds. Extract Actions library

---
+++ MoisturingSystem/ActionsList.cpp
@@ -0,0 +1,277 @@
+// 
+// 
+// 
+
+#include "ActionsList.h"
+
+// end of structures
+
+// Queue
+
+int indexOfAction(ActionsList* list, Action* a) {
+	for (int i = 0; i < (*list).availableActionsCount; i++) {
+		if (a == &(*list).availableActions[i]) {
+			return i;
+		}
+	}
+
+	return -1;
+}
+
+Action* find(ActionsList* list, Action* item) {
+	Action* cur = (*list).first;
+
+	while (cur != nullptr) {
+		if (cur == item) {
+			return cur;
+		}
+		cur = (*cur).next;
+	}
+	return nullptr;
+}
+
+#ifdef DEBUG
+
+void printList(ActionsList* list) {
+	Action* cur = (*list).first;
+	while (cur != nullptr) {
+		Serial.print(F(" Item: "));
+		Serial.print(indexOfAction(list, cur));
+		Serial.flush();
+		cur = (*cur).next;
+	}
+
+	Serial.println();
+}
+
+#endif
+
+Action* addListItem(ActionsList* list, Action* a) {
+
+	if (a != nullptr) {
+
+		(*a).next = nullptr;
+		(*a).prev = (*list).last;
+
+		if ((*list).last != nullptr) {
+			(*(*list).last).next = a;
+		}
+
+		(*list).last = a;
+
+		if ((*list).first == nullptr) {
+			(*list).first = a;
+		}
+
+		(*list).count++;
+#ifdef DEBUG
+		printList(list);
+#endif
+		return a;
+	}
+	else {
+#ifdef DEBUG
+		Serial.println(F("ITEM: null"));
+#endif
+	}
+
+	return nullptr;
+}
+
+bool removeListItem(ActionsList* list, Action* item) {
+
+	Action* tr = find(list, item);
+
+	if (tr == nullptr) {
+		return false;
+	}
+
+	if ((*list).last == tr) {
+		(*list).last = (*tr).prev;
+	}
+
+	if ((*list).first == tr) {
+		(*list).first = (*tr).next;
+	}
+
+	Action* pr = (*tr).prev;
+	Action* nx = (*tr).next;
+
+	if (pr != nullptr) {
+		(*pr).next = nx;
+	}
+
+	if (nx != nullptr) {
+		(*nx).prev = pr;
+	}
+
+	(*tr).prev = nullptr;
+	(*tr).next = nullptr;
+
+	(*list).count--;
+#ifdef DEBUG
+	printList(list);
+#endif
+	return true;
+}
+
+
+bool requestStop(ActionsList* list, Action* a) {
+	Action* item = find(list, a);
+	if (item != nullptr) {
+		(*a).stopRequested = true;
+		return true;
+	}
+
+	return false;
+}
+
+bool canStart(Action* a, unsigned long time) {
+
+	if ((*a).stopRequested) {
+		return false;
+	}
+
+	if ((*a).state == MS_RUNNING) {
+		return false;
+	}
+
+	if ((*a).ti > 0 && (*a).lst > 0 && (time - (*a).lst < (*a).ti)) {
+		return false;
+	}
+
+	return true;
+}
+
+bool shouldStop(Action* a, unsigned long time) {
+
+	if ((*a).stopRequested) {
+#ifdef DEBUG
+		Serial.print(F("Stop req"));
+#endif
+		return true;
+	}
+
+	if ((*a).state == MS_RUNNING) {
+		if ((*a).td > 0 && (*a).st > 0 && (time - (*a).st >= (*a).td)) {
+			return true;
+		}
+	}
+
+	return false;
+}
+
+bool descheduleAction(ActionsList* list, Action* a) {
+	Action* found = find(list, a);
+	if (found == nullptr) {
+		return false;
+	}
+
+	if (found != nullptr && (*found).state != MS_RUNNING) {
+		(*found).state = MS_NON_ACTIVE;
+
+		removeListItem(list, found);
+
+		return true;
+	}
+
+	return false;
+}
+
+bool scheduleAction(ActionsList* list, Action* a) {
+	Action* found = find(list, a);
+	if (found != nullptr) {
+		return false;
+	}
+
+	if ((*a).state != MS_NON_ACTIVE) {
+		return false;
+	}
+
+	if (a != nullptr) {
+		(*a).state = MS_SCHEDULED;
+		addListItem(list, a);
+		return true;
+	}
+
+	return false;
+}
+
+void doQueueActions(ActionsList* executionList) {
+	Action* action = (*executionList).first;
+	unsigned long time = millis();
+	int count = (*executionList).count;
+
+	int i = 0;
+	while (action != nullptr) {
+		if (canStart(action, time)) {
+#ifdef DEBUG
+			Serial.print(F("Starting: "));
+			Serial.print(indexOfAction(executionList, action));
+			Serial.println();
+#endif
+
+			(*action).start(action);
+			(*action).state = MS_RUNNING;
+			(*action).st = time;
+
+			Action* child = (*action).child;
+			while (child != nullptr) {
+#ifdef DEBUG
+				Serial.print(F("Starting child: "));
+				Serial.println(indexOfAction(executionList, child));
+#endif
+				(*child).start(child);
+				(*child).state = MS_CHILD_RUNNING;
+				child = (*child).child;
+			}
+		}
+		else {
+			if (shouldStop(action, time)) {
+
+#ifdef DEBUG
+				Serial.print(F("Stopping: "));
+				Serial.print(indexOfAction(executionList, action));
+				Serial.println();
+#endif
+
+				Action* child = (*action).child;
+				while (child != nullptr) {
+#ifdef DEBUG
+					Serial.print(F("Stopping child: "));
+					Serial.println(indexOfAction(executionList, child));
+#endif
+					(*child).stop(child);
+					(*child).state = MS_NON_ACTIVE;
+					child = (*child).child;
+				}
+
+				(*action).stop(action);
+				(*action).lst = time;
+				(*action).state = MS_PENDING;
+				(*action).clear = !(*action).frozen;
+				(*action).stopRequested = false;
+			}
+			else {
+				if ((*action).state == MS_RUNNING) {
+					if (time - (*action).st > (*action).to) {
+						(*action).tick(action);
+					}
+				}
+			}
+		}
+
+		action = (*action).next;
+	}
+
+	// Iterate over the available actions and stop the
+	// appropriate ones
+	for (int i = 0; i < (*executionList).availableActionsCount; i++) {
+		Action* a = &(*executionList).availableActions[i];
+		if ((*a).clear) {
+			descheduleAction(executionList, a);
+			(*a).clear = false;
+		}
+	}
+}
+
---
+++ MoisturingSystem/ActionsList.h
@@ -0,0 +1,63 @@
+// ActionsList.h
+
+#ifndef _ACTIONSLIST_h
+#define _ACTIONSLIST_h
+
+#if defined(ARDUINO) && ARDUINO >= 100
+#include "arduino.h"
+#else
+#include "WProgram.h"
+#endif
+
+
+#endif
+
+typedef enum ActionState {
+	MS_NON_ACTIVE = 0,
+	MS_SCHEDULED = 1,
+	MS_PENDING = 2,
+	MS_RUNNING = 3,
+	MS_CHILD_RUNNING = 4,
+	MS_CHILD_PENDING = 5
+};
+
+typedef struct Action {
+	Action* next = nullptr;
+	Action* prev = nullptr;
+	bool clear;
+	bool frozen = false; // once scheduled the action never expires
+	bool stopRequested = false;
+	unsigned long ti = 0; // interval between executions
+	unsigned long td = 1000; // maximum duration interval
+	unsigned long lst = -1; // last stop time
+	unsigned long to = 0; // first tick offset from start
+	unsigned long st = 0; // last start time
+	ActionState state = MS_NON_ACTIVE;
+	void (*tick)(Action*);
+	void (*start)(Action*);
+	void (*stop)(Action*);
+	Action* child = nullptr; // a child action; Child actions are activated after the master action has been activated;
+	   // child actions are deactivated before the master action is deactivated;
+	   // child actions are not part of the execution queue
+};
+
+typedef struct ActionsList {
+	size_t count = 0;
+	size_t availableActionsCount = 0;
+	Action* first = nullptr;
+	Action* last = nullptr;
+	Action* availableActions = nullptr;
+};
+
+int indexOfAction(ActionsList* list, Action* a);
+Action* find(ActionsList* list, Action* item);
+void printList(ActionsList* list);
+Action* addListItem(ActionsList* list, Action* a);
+bool removeListItem(ActionsList* list, Action* item);
+bool requestStop(ActionsList* list, Action* a);
+bool shouldStop(Action* a, unsigned long time);
+bool descheduleAction(ActionsList* list, Action* a);
+bool scheduleAction(ActionsList* list, Action* a);
+void doQueueActions(ActionsList* executionList);
+
+
